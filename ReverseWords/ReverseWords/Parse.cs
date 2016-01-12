6933b9fd5798f77092d5dbf0143ad6ac5973903a
bachmeyer <wesyb@yahoo.com>
2016-01-11 20:24:56 -0600

    Changing array string to read backwards

---
+++ ReverseWords/ReverseWords/Parse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReverseWords
+{
+    internal class Parse
+    {
+        private string v;
+
+        public Parse(string v)
+        {
+            this.v = v;
+        }
+
+        internal static object Equals()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ReverseWords/ReverseWords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your sentance:");
+            var userSent = (Console.ReadLine());
+            Console.WriteLine(userSent);
+            Console.ReadLine();
+            var array = userSent.Split(' ');
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                
+                Console.WriteLine(array[i]);
+                Console.ReadLine();
+            }
+
+
+
+
+
+        }
+    }
+}

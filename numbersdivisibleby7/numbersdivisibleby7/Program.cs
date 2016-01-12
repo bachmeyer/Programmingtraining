204636230f837fd1a1bb39f1b34a3e07f55e838e
bachmeyer <wesyb@yahoo.com>
2016-01-11 20:26:06 -0600

    A simple program that lists numbers divisible by 7 from 1 to 100

---
+++ numbersdivisibleby7/numbersdivisibleby7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numbersdivisibleby7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
